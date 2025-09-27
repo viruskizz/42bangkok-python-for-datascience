c3459037d006d40ca42f82f09a9b5bede30d50b4
Thitiwut Somsa <kizzaraiva@gmail.com>
2025-09-27 21:33:19 +0700

    ex00 ex01

---
+++ python-0/ex01/format_ft_time.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+now = datetime.now()
+ts = now.timestamp()
+print(f'Seconds since January 1, 1970: {ts:,.4f} or {ts:.2e} in scientific notation')
+print(now.strftime("%b %d %Y"))
