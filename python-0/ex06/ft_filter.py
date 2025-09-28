75bca85276abcf24fe84a92fcf337cd13c859cc4
Thitiwut Somsa <kizzaraiva@gmail.com>
2025-09-28 14:34:26 +0700

    python0-ex06-1-iter

--- python-0/ex06/ft_filter.py
+++ python-0/ex06/ft_filter.py
@@ -1,17 +1,19 @@
 from typing import Callable, Iterable
 
-def ft_filter(fn: Callable, lst: Iterable) -> list:
+def ft_filter(fn: Callable, lst: Iterable):
     """ My customize filter that clone from filter build-in-function """
     assert type(lst).__name__ == "list", "argument is not iterable"
     if fn is None:
         return lst
     assert type(fn).__name__ == "function", "argument is not function"
-    return [x for x in lst if fn(x)]
+    return iter([x for x in lst if fn(x)])
 
 def main():
     nums = [1, 2, 3, 4, 5, 6]
     filt = ft_filter(lambda x: x, nums)
-    print(filt)
+    f = filter(lambda x: x, nums)
+    print(type(filt))
+    print(list(filt))
 
 if __name__ == "__main__":
     main()
